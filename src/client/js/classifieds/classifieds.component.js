b5bfcb09d119653c1233294380867e7f1a94a45c
Matthew Gordon <laxredemption5525@gmail.com>
2017-01-30 12:57:16 -0700

    Add  dependency, make initial GET request for data

---
+++ src/client/js/classifieds/classifieds.component.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+
+  angular.module('app')
+    .component('listings', {
+      templateUrl: './js/classifieds/classifieds.template.html',
+      controller: ListingsController
+    });
+    ListingsController.$inject = ['$http'];
+    function ListingsController($http) {
+      let vm = this;
+
+      vm.$onInit = () => {
+        $http.get('/classifieds')
+        .then((response) => {
+          vm.data = response.data;
+          console.log(vm.data);
+        })
+        .catch((error) => {
+          next(error);
+        })
+      };
+
+    }
+}());
