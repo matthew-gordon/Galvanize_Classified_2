2541d70cc48a4774871a5dad32b78a9514652f92
Matthew Gordon <laxredemption5525@gmail.com>
2017-01-30 11:30:00 -0700

    Wire up client with static index

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const path = require('path');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const messages = require('./src/server/routes/classifieds');
+
+app.use('/', express.static(path.join(__dirname + '/src/client')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
